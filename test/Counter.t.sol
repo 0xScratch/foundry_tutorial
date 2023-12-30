0c442a42cbd0135689c8bd8477e343d27171b716
Aryan Malik <aryanhunny98@gmail.com>
2023-12-30 19:19:31 +0530

    Wrote some tests

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,3 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "forge-std/Test.sol";
+import "src/Counter.sol";
+
+contract ContractTest is Test {
+    Counter count;
+
+    function setUp() public {
+        count = new Counter(5);
+    }
+
+    function testGetCount() public {
+        int value = count.getCount();
+        assertEq(value, 5);
+    }
+
+    function testIncrement() public {
+        count.incrementCount();
+        int value = count.getCount();
+        assertEq(value, 6);
+        emit log_named_int("The value is", value);
+    }
+}
